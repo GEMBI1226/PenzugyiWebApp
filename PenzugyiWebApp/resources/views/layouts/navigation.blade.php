9d96bec5c0cff1c78a4aa3288375a86889f049d7
Gembiczki Dániel <gembi1226@gmail.com>
2025-11-28 18:07:59 +0100

    fix: navbar

--- PenzugyiWebApp/resources/views/layouts/navigation.blade.php
+++ PenzugyiWebApp/resources/views/layouts/navigation.blade.php
@@ -53,7 +53,7 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="flex space-x-8 -my-px ml-10">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
